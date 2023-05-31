79afb97f167546fd4e0f120b20c385dc05080628
Er-Carl Abban <102951384+KharlAbban@users.noreply.github.com>
2023-05-31 13:55:37 +0100

    Changed pages layout

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -4,7 +4,7 @@
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
   const forms = document.querySelectorAll('.needs-validation')
-
+  
   // Loop over them and prevent submission
   Array.from(forms).forEach(form => {
     form.addEventListener('submit', event => {
@@ -12,24 +12,8 @@
         event.preventDefault()
         event.stopPropagation()
       }
-
       form.classList.add('was-validated')
     }, false)
   })
 })()
-
-document.querySelectorAll(".signIn").forEach(element => {
-  element.addEventListener("click", (e) => {
-  e.preventDefault();
-  document.querySelector(".card-title").textContent = "Sign In";
-  document.querySelector("#registerDiv").classList.add('d-none');
-  document.querySelector("#signInDiv").classList.remove('d-none');
-});
-});
 document.querySelector("#year").textContent = new Date().getFullYear();
-document.querySelector("#register").addEventListener("click", (e) => {
-  e.preventDefault();
-  document.querySelector(".card-title").textContent = "Registration Form";
-  document.querySelector("#registerDiv").classList.remove('d-none');
-  document.querySelector("#signInDiv").classList.add('d-none');
-});
